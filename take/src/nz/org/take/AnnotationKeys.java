5bce094adf00210767cbb0e29490eacee44356c8
jens.dietrich <jens.dietrich@7b52cedb-bc2e-0410-92dc-b5e457b4923a>
2007-04-29 09:05:34 +0000

    renames annotation keys to @take.compilerhint

--- take/src/nz/org/take/AnnotationKeys.java
+++ take/src/nz/org/take/AnnotationKeys.java
@@ -2,9 +2,9 @@ package nz.org.take;
 
 public interface AnnotationKeys {
 	// annotation on queries used to define the method
-	public static final String TAKE_GENERATE_SLOTS = "take.generate.slots";
+	public static final String TAKE_GENERATE_SLOTS = "take.compilerhint.slots";
 	// annotation on queries used to define the method
-	public static final String TAKE_GENERATE_METHOD = "take.generate.method";
+	public static final String TAKE_GENERATE_METHOD = "take.compilerhint.method";
 	// annotation on queries used to define the local class name
-	public static final String TAKE_GENERATE_CLASS = "take.generate.class";
+	public static final String TAKE_GENERATE_CLASS = "take.compilerhint.class";
 }
