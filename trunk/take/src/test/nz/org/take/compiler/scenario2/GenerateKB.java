eac2e5c866d6e7af7cbb3bac19b245981a6863e9
jens.dietrich <jens.dietrich@7b52cedb-bc2e-0410-92dc-b5e457b4923a>
2007-04-29 23:46:30 +0000

    added test scenario 2 - contains test cases for derivation tracing

---
+++ trunk/take/src/test/nz/org/take/compiler/scenario2/GenerateKB.java
@@ -0,0 +1,114 @@
+/*
+ * Copyright (C) 2007 <A href="http://www-ist.massey.ac.nz/JBDietrich" target="_top">Jens Dietrich</a>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+
+package test.nz.org.take.compiler.scenario2;
+
+import java.io.StringReader;
+import java.util.ArrayList;
+import java.util.List;
+import org.apache.log4j.BasicConfigurator;
+import nz.org.take.KnowledgeBase;
+import nz.org.take.compiler.NameGenerator;
+import nz.org.take.compiler.reference.DefaultCompiler;
+import nz.org.take.compiler.util.DefaultLocation;
+import nz.org.take.compiler.util.DefaultNameGenerator;
+import nz.org.take.compiler.util.jalopy.JalopyCodeFormatter;
+import nz.org.take.script.KnowledgeBaseReader;
+import nz.org.take.script.ScriptException;
+
+/**
+ * Script to generate a KB.
+ * @author <a href="http://www-ist.massey.ac.nz/JBDietrich/">Jens Dietrich</a>
+ */
+
+public class GenerateKB {
+	public static final String ROOT = "r";
+	public static final int DEPTH=3;
+		
+		/**
+		 * Generate the sources for the example.
+		 * @param args
+		 */
+		public static KnowledgeBase buildKB() throws Exception {
+			BasicConfigurator.configure();			
+			// generate kb
+			KnowledgeBaseReader reader = new KnowledgeBaseReader();
+			KnowledgeBase kb = reader.read(new StringReader(buildScript()));
+			return kb;
+		}
+		/**
+		 * Generate the script. 
+		 */
+		private static String buildScript() {
+			StringBuffer b = new StringBuffer(); 
+			
+			appendLine(b,"// script for test scenario 1");
+			appendLine(b,"@@dc:date=30/02/2007");
+			appendLine(b,"@@dc:creator=author jens dietrich");
+			
+			// variables
+			appendLine(b,"var java.lang.String descendant,ancestor,x");
+			
+			// queries
+			appendLine(b,"@take.compilerhint.slots=person1,person2");
+			appendLine(b,"@take.compilerhint.method=isAncestor");
+			appendLine(b,"@take.compilerhint.class=AncestorRelationship");
+			appendLine(b,"query isAncestor(in,in)");
+			
+			// rules
+			appendLine(b,"rule1: if isFather(descendant,ancestor) then isAncestor(descendant,ancestor)");
+			appendLine(b,"rule2: if isAncestor(x,ancestor) and isFather(descendant,x) then isAncestor(descendant,ancestor)");
+			
+			// facts
+			List<List<String>> generations = new ArrayList<List<String>>();
+			int counter = 0;
+			for (int i=0;i<DEPTH;i++) {
+				List<String> previousGeneration = null;
+				if (i==0) {
+					previousGeneration = new ArrayList<String>();
+					previousGeneration.add(ROOT);
+					generations.add(previousGeneration);
+				}
+				else 
+					previousGeneration = generations.get(generations.size()-1);
+				
+				List<String> newGeneration = new ArrayList<String>();
+				for (String ancestor:previousGeneration) {
+					for (int j=0;j<2;j++) {
+						counter = counter+1;
+						String descendant = ancestor+j;
+						newGeneration.add(descendant);
+						appendLine(b,"fact"+counter,": ","isFather(\"",descendant,"\",\"",ancestor,"\")");
+					}
+				}
+				generations.add(newGeneration);
+				
+			}
+			
+			return b.toString();
+		}
+		private static void appendLine(StringBuffer b,String...strings ){
+			for (String s:strings) {
+				b.append(s);
+				System.out.print(s);
+			}
+			b.append('\n');
+			System.out.println();
+		}
+}
---
+++ trunk/take/src/test/nz/org/take/compiler/scenario2/Tests.java
@@ -0,0 +1,201 @@
+/*
+ * Copyright (C) 2007 <A href="http://www-ist.massey.ac.nz/JBDietrich" target="_top">Jens Dietrich</a>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+package test.nz.org.take.compiler.scenario2;
+
+import java.util.*;
+
+import nz.org.take.Constant;
+import nz.org.take.DerivationRule;
+import nz.org.take.Fact;
+import nz.org.take.KnowledgeBase;
+import nz.org.take.KnowledgeElement;
+import nz.org.take.rt.ResultSet;
+import test.nz.org.take.compiler.scenario2.generated.*;
+import junit.framework.TestCase;
+
+/**
+ * Tests for this scenario. 
+ * For now, the classes have to generated and compiled manually.
+ * For generation, use the script GenerateClasses.
+ * @author <a href="http://www-ist.massey.ac.nz/JBDietrich/">Jens Dietrich</a>
+ */
+
+public class Tests extends TestCase
+{
+	private KnowledgeBase kb = null;
+	/**
+	 * Construct new test instance	 *
+	 * @param name the test name
+	 */
+	public Tests(String name)
+	{
+		super(name);
+	}
+
+	/**
+	 * Perform pre-test initialization
+	 *
+	 * @throws Exception
+	 *
+	 * @see TestCase#setUp()
+	 */
+	protected void setUp() throws Exception
+	{
+		kb = GenerateKB.buildKB();
+	}
+
+	/**
+	 * Perform post-test clean up
+	 *
+	 * @throws Exception
+	 *
+	 * @see TestCase#tearDown()
+	 */
+	protected void tearDown()
+		throws Exception
+	{
+		super.tearDown();
+		// Add additional tear down code here
+	}
+
+	public void test1(){
+		
+		System.out.println("starting test case 1");
+		
+		_KB kb = new _KB();
+		nz.org.take.rt.ResultSet<AncestorRelationship> results = kb.isAncestor("r0","r");	
+		assertTrue(results.hasNext());
+		AncestorRelationship next = results.next();
+		List<String> x = results.getDerivationLog();
+		results.getDerivationController().printLog();
+		assertEquals("Wrong number of rules",1,countRules(x));
+		assertEquals("Wrong number of facts",1,countFacts(x));
+		assertTrue("Expected fact not found",checkFatherFact(x,"r0","r"));
+	}
+	
+	public void test2(){
+		
+		System.out.println("starting test case 2");
+		
+		_KB kb = new _KB();
+		ResultSet<AncestorRelationship> results = kb.isAncestor("r1","r");	
+		assertTrue(results.hasNext());
+		AncestorRelationship next = results.next();
+		List<String> x = results.getDerivationLog();
+		results.getDerivationController().printLog();
+		assertEquals("Wrong number of rules",1,countRules(x));
+		assertEquals("Wrong number of facts",1,countFacts(x));
+		assertTrue("Expected fact not found",checkFatherFact(x,"r1","r"));
+	}
+	
+	public void test3(){
+		
+		System.out.println("starting test case 3");
+		
+		_KB kb = new _KB();
+		ResultSet<AncestorRelationship> results = kb.isAncestor("r00","r");	
+		assertTrue(results.hasNext());
+		AncestorRelationship next = results.next();
+		List<String> x = results.getDerivationLog();
+		results.getDerivationController().printLog();
+		assertEquals("Wrong number of rules",2,countRules(x));
+		assertEquals("Wrong number of facts",2,countFacts(x));
+		assertTrue("Expected fact not found",checkFatherFact(x,"r0","r"));
+		assertTrue("Expected fact not found",checkFatherFact(x,"r00","r0"));
+	}
+	
+	public void test4(){
+		
+		System.out.println("starting test case 4");
+		
+		_KB kb = new _KB();
+		ResultSet<AncestorRelationship> results = kb.isAncestor("r11","r");	
+		assertTrue(results.hasNext());
+		AncestorRelationship next = results.next();
+		List<String> x = results.getDerivationLog();
+		results.getDerivationController().printLog();
+		assertEquals("Wrong number of rules",2,countRules(x));
+		assertEquals("Wrong number of facts",2,countFacts(x));
+		assertTrue("Expected fact not found",checkFatherFact(x,"r1","r"));
+		assertTrue("Expected fact not found",checkFatherFact(x,"r11","r1"));
+	}
+	
+	public void test5(){
+		
+		System.out.println("starting test case 5");
+		
+		_KB kb = new _KB();
+		ResultSet<AncestorRelationship> results = kb.isAncestor("r000","r");	
+		assertTrue(results.hasNext());
+		AncestorRelationship next = results.next();
+		List<String> x = results.getDerivationLog();
+		results.getDerivationController().printLog();
+		assertEquals("Wrong number of rules",2,countRules(x));
+		assertEquals("Wrong number of facts",3,countFacts(x));
+		assertTrue("Expected fact not found",checkFatherFact(x,"r0","r"));
+		assertTrue("Expected fact not found",checkFatherFact(x,"r00","r0"));
+		assertTrue("Expected fact not found",checkFatherFact(x,"r000","r00"));
+	}
+	
+	
+	private boolean checkFatherFact(List<String> list,String name1,String name2) {
+		for (String id:list) {
+			KnowledgeElement e = kb.getElement(id);
+			if (e instanceof Fact) {
+				Fact f = (Fact)e;
+				boolean ok = "isFather".equals(f.getPredicate().getName());
+				ok = ok && f.getTerms().length==2;
+				ok = ok && ((Constant)f.getTerms()[0]).getObject().equals(name1);
+				ok = ok && ((Constant)f.getTerms()[1]).getObject().equals(name2);
+				if (ok)
+					return true;
+			}
+		}
+		return false;
+	}
+	private int countRules(List<String> x) {
+		Set set = new HashSet(); // remove duplicates
+		set.addAll(x);
+		int count = 0;
+		for (Object e:set) {
+			String r = e.toString();
+			if (isRule(r))
+				count = count+1;
+		}
+		return count;
+	} 
+	private int countFacts(List<String> x) {
+		Set set = new HashSet(); // remove duplicates
+		set.addAll(x);
+		int count = 0;
+		for (Object e:set) {
+			String r = e.toString();
+			if (isFact(r))
+				count = count+1;
+		}
+		return count;
+	} 
+	private boolean isRule(String id) {
+		return kb.getElement(id) instanceof DerivationRule;
+	}
+	private boolean isFact(String id) {
+		return kb.getElement(id) instanceof Fact;
+	}
+}
+	
