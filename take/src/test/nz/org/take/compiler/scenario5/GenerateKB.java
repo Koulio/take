d6d81d2d16f7c935efa0e17b567122489c4c0c8f
jens.dietrich <jens.dietrich@7b52cedb-bc2e-0410-92dc-b5e457b4923a>
2007-05-14 10:04:05 +0000

    started example for constants

---
+++ take/src/test/nz/org/take/compiler/scenario5/GenerateKB.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright (C) 2007 <A href="http://www-ist.massey.ac.nz/JBDietrich" target="_top">Jens Dietrich</a>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+
+package test.nz.org.take.compiler.scenario5;
+
+import java.io.FileReader;
+import org.apache.log4j.BasicConfigurator;
+import nz.org.take.KnowledgeBase;
+import nz.org.take.script.KnowledgeBaseReader;
+
+/**
+ * Script to generate a KB.
+ * @author <a href="http://www-ist.massey.ac.nz/JBDietrich/">Jens Dietrich</a>
+ */
+
+public class GenerateKB {
+		
+		/**
+		 * Generate the sources for the example.
+		 * @param args
+		 */
+		public static KnowledgeBase buildKB() throws Exception {
+			BasicConfigurator.configure();			
+			// generate kb
+			KnowledgeBaseReader reader = new KnowledgeBaseReader();
+			KnowledgeBase kb = reader.read(new FileReader("src/test/nz/org/take/compiler/scenario5/rules5.take"));
+			return kb;
+		}
+}
