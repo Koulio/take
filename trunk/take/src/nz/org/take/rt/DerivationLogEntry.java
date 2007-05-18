6704fc85e28d7e746633ab0a235494431359b8af
jens.dietrich <jens.dietrich@7b52cedb-bc2e-0410-92dc-b5e457b4923a>
2007-05-18 03:45:57 +0000

    started to change derivation logging to keep track of the object used in the derivation not yet complete - still have to refactor some test cases

---
+++ trunk/take/src/nz/org/take/rt/DerivationLogEntry.java
@@ -0,0 +1,50 @@
+/*
+ * Copyright (C) 2007 <A href="http://www-ist.massey.ac.nz/JBDietrich" target="_top">Jens Dietrich</a>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+
+package nz.org.take.rt;
+
+/**
+ * Represents one node in the derivation.
+ * Contains the name of the artefact used (can be used to look up rules in the kb),
+ * and an int defining what kind of object this is.
+ * See the constants in DerivationController for possible values.
+ * @author <a href="http://www-ist.massey.ac.nz/JBDietrich/">Jens Dietrich</a>
+ */
+
+public class DerivationLogEntry {
+	private String name = null;
+	private int kind = 0;
+	public DerivationLogEntry(String name, int kind) {
+		super();
+		this.name = name;
+		this.kind = kind;
+	}
+	public int getKind() {
+		return kind;
+	}
+	public void setKind(int kind) {
+		this.kind = kind;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+}
