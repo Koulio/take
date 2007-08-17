3ca14f911bedec7a37da4ad81d4c46cff18c1056
jens.dietrich <jens.dietrich@7b52cedb-bc2e-0410-92dc-b5e457b4923a>
2007-08-17 09:27:32 +0000

    fixed test case AE04

--- trunk/take/src/example/nz/org/take/compiler/userv/testcases/UservTestCases.java
+++ trunk/take/src/example/nz/org/take/compiler/userv/testcases/UservTestCases.java
@@ -22,49 +22,8 @@ public class UservTestCases extends TestCase {
 			System.out.println(((DerivationLogEntry)s).getName());
 		}
 	}
-	public void testAE_PTC01() throws Exception {
-		Car car = new Car();
-		car.setConvertible(true);
-		car.setPrice(20000);
-		ResultSet<PotentialTheftRating> result = kb.getPotenialTheftRating(car);
-		assertTrue(result.hasNext());
-		assertEquals("high",result.next().rating);
-	}
-	public void testAE_PTC02() throws Exception {
-		Car car = new Car();
-		car.setConvertible(false);
-		car.setPrice(50000);
-		ResultSet<PotentialTheftRating> result = kb.getPotenialTheftRating(car);
-		assertTrue(result.hasNext());
-		assertEquals("high",result.next().rating);
-	}
-	public void testAE_PTC03() throws Exception {
-		Car car = new Car();
-		car.setConvertible(false);
-		car.setPrice(20000);
-		car.setType("Mini");
-		ResultSet<PotentialTheftRating> result = kb.getPotenialTheftRating(car);
-		assertTrue(result.hasNext());
-		assertEquals("high",result.next().rating);
-	}
-	public void testAE_PTC04() throws Exception {
-		Car car = new Car();
-		car.setConvertible(false);
-		car.setPrice(30000);
-		car.setType("BMW 3");
-		ResultSet<PotentialTheftRating> result = kb.getPotenialTheftRating(car);
-		assertTrue(result.hasNext());
-		assertEquals("moderate",result.next().rating);
-	}
-	public void testAE_PTC05() throws Exception {
-		Car car = new Car();
-		car.setConvertible(false);
-		car.setPrice(18000);
-		car.setType("Daihatsu Sirion");
-		ResultSet<PotentialTheftRating> result = kb.getPotenialTheftRating(car);
-		assertTrue(result.hasNext());
-		assertEquals("low",result.next().rating);
-	}
+
+
 	public void testAE_POIC01() throws Exception {
 		Car car = new Car();
 		car.setHasDriversAirbag(false);
@@ -74,6 +33,7 @@ public class UservTestCases extends TestCase {
 		assertTrue(result.hasNext());
 		assertEquals("extremely high",result.next().rating);
 	}
+
 	public void testAE_POIC02() throws Exception {
 		Car car = new Car();
 		car.setHasDriversAirbag(true);
@@ -83,6 +43,7 @@ public class UservTestCases extends TestCase {
 		assertTrue(result.hasNext());
 		assertEquals("high",result.next().rating);
 	}
+
 	public void testAE_POIC03() throws Exception {
 		Car car = new Car();
 		car.setHasDriversAirbag(true);
@@ -92,6 +53,7 @@ public class UservTestCases extends TestCase {
 		assertTrue(result.hasNext());
 		assertEquals("moderate",result.next().rating);
 	}
+
 	public void testAE_POIC04() throws Exception {
 		Car car = new Car();
 		car.setHasDriversAirbag(true);
@@ -101,6 +63,7 @@ public class UservTestCases extends TestCase {
 		assertTrue(result.hasNext());
 		assertEquals("low",result.next().rating);
 	}
+
 	public void testAE_POIC05() throws Exception {
 		Car car = new Car();
 		car.setConvertible(true);
@@ -109,6 +72,55 @@ public class UservTestCases extends TestCase {
 		assertTrue(result.hasNext());
 		assertEquals("extremely high",result.next().rating);
 	}
+
+	public void testAE_PTC01() throws Exception {
+		Car car = new Car();
+		car.setConvertible(true);
+		car.setPrice(20000);
+		ResultSet<PotentialTheftRating> result = kb.getPotenialTheftRating(car);
+		assertTrue(result.hasNext());
+		assertEquals("high",result.next().rating);
+	}
+
+	public void testAE_PTC02() throws Exception {
+		Car car = new Car();
+		car.setConvertible(false);
+		car.setPrice(50000);
+		ResultSet<PotentialTheftRating> result = kb.getPotenialTheftRating(car);
+		assertTrue(result.hasNext());
+		assertEquals("high",result.next().rating);
+	}
+
+	public void testAE_PTC03() throws Exception {
+		Car car = new Car();
+		car.setConvertible(false);
+		car.setPrice(20000);
+		car.setType("Mini");
+		ResultSet<PotentialTheftRating> result = kb.getPotenialTheftRating(car);
+		assertTrue(result.hasNext());
+		assertEquals("high",result.next().rating);
+	}
+
+	public void testAE_PTC04() throws Exception {
+		Car car = new Car();
+		car.setConvertible(false);
+		car.setPrice(30000);
+		car.setType("BMW 3");
+		ResultSet<PotentialTheftRating> result = kb.getPotenialTheftRating(car);
+		assertTrue(result.hasNext());
+		assertEquals("moderate",result.next().rating);
+	}
+
+	public void testAE_PTC05() throws Exception {
+		Car car = new Car();
+		car.setConvertible(false);
+		car.setPrice(18000);
+		car.setType("Daihatsu Sirion");
+		ResultSet<PotentialTheftRating> result = kb.getPotenialTheftRating(car);
+		assertTrue(result.hasNext());
+		assertEquals("low",result.next().rating);
+	}
+
 	public void testAE01() throws Exception {
 		Car car = new Car();
 		car.setHasDriversAirbag(false);
@@ -118,6 +130,7 @@ public class UservTestCases extends TestCase {
 		assertTrue(result.hasNext());
 		assertEquals("not eligible",result.next().value);
 	}
+
 	public void testAE02() throws Exception {
 		Car car = new Car();
 		car.setHasDriversAirbag(true);
@@ -128,6 +141,7 @@ public class UservTestCases extends TestCase {
 		assertEquals("provisional",result.next().value);
 		printLog(result);
 	}
+
 	public void testAE03() throws Exception {
 		Car car = new Car();
 		car.setConvertible(true);
@@ -140,7 +154,7 @@ public class UservTestCases extends TestCase {
 	}
 	public void testAE04() throws Exception {
 		Car car = new Car();
-		car.setConvertible(true);
+		car.setConvertible(false);
 		car.setPrice(18000);
 		car.setType("Skoda Fabia");
 		car.setHasDriversAirbag(true);
