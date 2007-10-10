c3f073cba45e49f2996fee563e61400890f6ca03
jens.dietrich <jens.dietrich@7b52cedb-bc2e-0410-92dc-b5e457b4923a>
2007-10-10 08:05:27 +0000

    added new EURent examples + tests added support for code generation for negated property predicates with 10 signature

---
+++ trunk/take/src/example/nz/org/take/compiler/eurent/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * Example based on the EURent scenario published by the EU business rule conference http://www.eurobizrules.org.
+ * Contains rules, the domain model and a scrupt to generate the KB.
+ */
+package example.nz.org.take.compiler.eurent;
