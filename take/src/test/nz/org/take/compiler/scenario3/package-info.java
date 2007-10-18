2266c082607a423cb7b2319f6be015d945e2a0af
jens.dietrich <jens.dietrich@7b52cedb-bc2e-0410-92dc-b5e457b4923a>
2007-10-18 09:29:30 +0000

    fixed issue 5 - all test cases running now added support to add default annotations automatically

--- take/src/test/nz/org/take/compiler/scenario3/package-info.java
+++ take/src/test/nz/org/take/compiler/scenario3/package-info.java
@@ -1,5 +1,5 @@
 /**
  * Test scenario that uses beans as types, and methods as functions and predicates.
- * Inheritence is also tested. 
+ * Inheritance is also tested. 
  */
 package test.nz.org.take.compiler.scenario3;
