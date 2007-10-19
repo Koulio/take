dd35ecca2929053175968b61eb4fb2ac265faf5f
jens.dietrich <jens.dietrich@7b52cedb-bc2e-0410-92dc-b5e457b4923a>
2007-10-19 03:10:48 +0000

    added classes

---
+++ trunk/RuleML2007_challenge/src/example/nz/org/take/compiler/userv/main/DerivationModel.java
@@ -0,0 +1,132 @@
+package example.nz.org.take.compiler.userv.main;
+/*
+ * Copyright (C) 2007 <A href="http://www-ist.massey.ac.nz/JBDietrich" target="_top">Jens Dietrich</a>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import javax.swing.event.TreeModelListener;
+import javax.swing.tree.TreeModel;
+import javax.swing.tree.TreePath;
+import example.nz.org.take.compiler.userv.generated.UservRules;
+import nz.org.take.rt.DerivationController;
+import nz.org.take.rt.DerivationLogEntry;
+
+
+/**
+ * Tree model used to visualise the rules used and their annotations.
+ * @author <a href="http://www-ist.massey.ac.nz/JBDietrich/">Jens Dietrich</a>
+ */
+
+public class DerivationModel  implements TreeModel {
+	private List<DerivationLogEntry> derivationLog = new ArrayList<DerivationLogEntry>();
+	private Map<String,List<Annotation>> annotations = new HashMap<String,List<Annotation>>();
+	private UservRules kb = new UservRules();
+	class Annotation {
+		String owner,key,value;
+		public Annotation(String owner, String key, String value) {
+			super();
+			this.owner = owner;
+			this.key = key;
+			this.value = value;
+		}
+		public String toString() {
+			return key+": "+value;
+		}
+	}
+	
+	public DerivationModel(List<DerivationLogEntry> log) {
+		
+		for (DerivationLogEntry e:log) {
+			if (e.getKind()==DerivationController.RULE || e.getKind()==DerivationController.FACT || e.getKind()==DerivationController.EXTERNAL_FACT_SET) {
+				derivationLog.add(e);
+			}
+		}
+	}
+
+
+	private List<Annotation> getAnnotations(String id) {
+		List<Annotation>  l = this.annotations.get(id);
+		if (l==null) {
+			l = new ArrayList<Annotation>();
+			Map<String,String> map = kb.getAnnotations(id);
+			if (map!=null) {
+				for (Entry<String,String> e:map.entrySet()) {
+					Annotation n = new Annotation(id,e.getKey(),e.getValue());
+					l.add(n);
+				}
+			}
+			annotations.put(id,l);
+		}
+		return l;
+	}
+	public void addTreeModelListener(TreeModelListener l) {
+	}
+
+	public Object getChild(Object parent, int index) {
+		if (parent==derivationLog) {
+			return derivationLog.get(index).getName();
+		}
+		else if (parent instanceof String) {
+			return getAnnotations((String)parent).get(index);
+		}
+		return null;
+	}
+
+	public int getChildCount(Object parent) {
+		if (parent==derivationLog) {
+			return derivationLog.size();
+		}
+		else if (parent instanceof String) { 
+			List<Annotation> l = getAnnotations((String)parent);
+			return l==null?0:l.size();
+		}
+		return 0;
+	}
+
+	@Override
+	public int getIndexOfChild(Object parent, Object child) {
+		if (parent==derivationLog)
+			return derivationLog.indexOf(child);
+		else if (parent instanceof String) {
+			return getAnnotations((String)parent).indexOf(child);
+		}
+		return 0;
+	}
+
+	@Override
+	public Object getRoot() {
+		return derivationLog;
+	}
+
+	public boolean isLeaf(Object node) {
+		return this.getChildCount(node)==0;
+	}
+
+	@Override
+	public void removeTreeModelListener(TreeModelListener l) {
+	}
+
+	@Override
+	public void valueForPathChanged(TreePath path, Object newValue) {
+	}
+
+
+}
---
+++ trunk/RuleML2007_challenge/src/example/nz/org/take/compiler/userv/main/MultiValueResultView.java
@@ -0,0 +1,111 @@
+package example.nz.org.take.compiler.userv.main;
+/*
+ * Copyright (C) 2007 <A href="http://www-ist.massey.ac.nz/JBDietrich" target="_top">Jens Dietrich</a>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.util.ArrayList;
+import java.util.List;
+import javax.swing.DefaultListModel;
+import javax.swing.JButton;
+import javax.swing.JList;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
+import nz.org.take.rt.DerivationLogEntry;
+import nz.org.take.rt.ResultSet;
+
+/**
+ * Component to visualise a result set with multiple results.
+ * @author <a href="http://www-ist.massey.ac.nz/JBDietrich/">Jens Dietrich</a>
+ */
+
+public abstract class MultiValueResultView extends JPanel {
+	private JList list = new JList();
+	private JButton logButton = new JButton(" ? ");
+	private List<Integer> values = new ArrayList<Integer>();
+	private List<List<DerivationLogEntry>> logs = new ArrayList<List<DerivationLogEntry>>();
+	
+	public MultiValueResultView() {
+		super();
+		setLayout(new BorderLayout(5,5));
+		add(new JScrollPane(list),BorderLayout.CENTER);
+		JPanel bPane = new JPanel(new FlowLayout(FlowLayout.RIGHT));
+		logButton.setBorder(null);
+		bPane.add(logButton);
+		add(bPane,BorderLayout.SOUTH);
+		logButton.addActionListener(
+				new ActionListener() {
+					public void actionPerformed(ActionEvent e) {
+						int pos = list.getSelectedIndex();
+						if (pos<logs.size()) {
+							DerivationLogViewer.displayUsedRules(logs.get(pos),MultiValueResultView.this);
+						}
+					}
+				}
+		);
+		logButton.setEnabled(false);
+		list.addListSelectionListener(
+				new ListSelectionListener() {
+					public void valueChanged(ListSelectionEvent e) {
+						int pos = list.getSelectedIndex();
+						logButton.setEnabled(pos<logs.size());
+					}
+				}
+		);
+		list.addMouseListener(
+				new MouseAdapter() {
+					// to open derivation log on double click
+					public void mouseClicked(MouseEvent e) {
+						int pos = list.getSelectedIndex();
+						if (e.getClickCount()>1 && pos<logs.size()) {
+							DerivationLogViewer.displayUsedRules(logs.get(pos),MultiValueResultView.this);
+						}
+					}
+				}
+		);
+		
+	}
+	
+	public abstract int extractValue(Object rs);
+	
+	public void setResultSet(ResultSet rs) {
+		int total = 0;
+		DefaultListModel model = new DefaultListModel();
+		values.clear();
+		logs.clear();
+		while (rs.hasNext()) {
+			int next = extractValue(rs.next());
+			total = total+next;
+			model.addElement(next);
+			values.add(next);
+			logs.add(rs.getDerivationLog());
+		}
+		rs.close();
+		model.addElement("total: " + total);
+		list.setModel(model);
+		logButton.setEnabled(false);
+	}
+	
+	
+}
