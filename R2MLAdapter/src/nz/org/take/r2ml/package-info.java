00cbbaf928289c03db2743c2809fd4ce06aa280a
bastian.schenke <bastian.schenke@7b52cedb-bc2e-0410-92dc-b5e457b4923a>
2007-11-26 12:46:27 +0000

    finally the build-script (forgot it in the last commit) added package-info.jar for the main r2ml-package

---
+++ R2MLAdapter/src/nz/org/take/r2ml/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * This package contains classes for the import of R2ML-files into the
+ * take-rule-compiler.
+ * R2ML is a markup language for rules defined by the REWERSE project.
+ */
+package nz.org.take.r2ml;
