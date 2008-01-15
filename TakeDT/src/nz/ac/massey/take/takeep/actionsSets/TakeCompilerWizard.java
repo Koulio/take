a7ab4e4b63417578590f343516f409c76320e7be
grahar64 <grahar64@7b52cedb-bc2e-0410-92dc-b5e457b4923a>
2008-01-15 21:14:14 +0000

    Complier window finished with dialogues for selection.

--- TakeDT/src/nz/ac/massey/take/takeep/actionsSets/TakeCompilerWizard.java
+++ TakeDT/src/nz/ac/massey/take/takeep/actionsSets/TakeCompilerWizard.java
@@ -1,46 +1,74 @@
 package nz.ac.massey.take.takeep.actionsSets;
 
+import java.util.LinkedList;
+import java.util.StringTokenizer;
+
+import org.eclipse.jface.layout.GridDataFactory;
 import org.eclipse.jface.wizard.Wizard;
 import org.eclipse.jface.wizard.WizardPage;
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.ControlAdapter;
 import org.eclipse.swt.layout.FillLayout;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Event;
 import org.eclipse.swt.widgets.Label;
-
+import org.eclipse.swt.widgets.Listener;
+import org.eclipse.swt.widgets.Text;
+import org.eclipse.jdt.internal.ui.wizards.SuperInterfaceSelectionDialog;
+import org.eclipse.jdt.ui.wizards.NewClassWizardPage;
+import org.eclipse.jdt.ui.wizards.NewTypeWizardPage;
 public class TakeCompilerWizard extends Wizard{
+
+
+
+	private TakeCompileWizardPanel wp;
+	
+
 
 	public TakeCompilerWizard() {
 		super();
 		this.setWindowTitle("Take Compiler Wizard");
 		this.setNeedsProgressMonitor(false);
-		
-		
-		WizardPage wp = new WizardPage("page 1"){
 
-			@Override
-			public void createControl(Composite parent) {
-				
-				Composite topLevel = new Composite(parent, SWT.NONE);
-			    topLevel.setLayout(new FillLayout());
 
-			    Label textLabel = new Label(topLevel, SWT.CENTER);
-			    textLabel.setText("dsadf");
-
-			    setControl(topLevel);
-			    setPageComplete(true);
-				
-			}};
-			this.addPage(wp);
+		wp = createWizardPage();
+		this.addPage(wp);
 	}
 
+	private TakeCompileWizardPanel createWizardPage() {
+		TakeCompileWizardPanel wp = new TakeCompileWizardPanel("TakeWizard");
+		wp.setPageComplete(true);
+		
+		return wp;
+	}
+
+
+	
 	@Override
 	public boolean performFinish() {
-		// TODO Auto-generated method stub
+
+		System.out.println("Package Name : " + wp.getPackageName());
+		System.out.println("Class Name : " + wp.getClassName());
+		System.out.println("Location : " + wp.getSourceOutputLocation());
+		
+		for(Object o : wp.getImportStatements())
+		{
+			System.out.println("import : " + o);
+		}
+		
+		for(Object o : wp.getAdditionalInterfaces())
+		{
+			System.out.println("interface : " + o);
+		}
+		
+		System.out.println("Auto annotate : " + wp.isAutoAnotate());
+		System.out.println("Source Tranform: " + wp.isSourceTransform());
 		return true;
 	}
 
-
-
-
-
 }
+
+	
