56f919da1c6ff3c00fa0cd45fcad44af3d4043d1
jens.dietrich <jens.dietrich@7b52cedb-bc2e-0410-92dc-b5e457b4923a>
2008-02-11 02:22:56 +0000

    released version

--- R2MLAdapter/examplesrc/example/nz/org/take/r2ml/eurent/package-info.java
+++
@@ -1,5 +0,0 @@
-/**
- * Example based on the EURent scenario published by the EU business rule conference http://www.eurobizrules.org.
- * Contains rules, the domain model and a R2ML file to generate the KB.
- */
-package example.nz.org.take.r2ml.eurent;
