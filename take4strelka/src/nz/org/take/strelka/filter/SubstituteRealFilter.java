a00185d772cf2e5a83394aef21c79409c6fc522a
bastian.schenke <bastian.schenke@7b52cedb-bc2e-0410-92dc-b5e457b4923a>
2008-05-13 20:03:22 +0000

    several bugfixes

--- take4strelka/src/nz/org/take/strelka/filter/SubstituteRealFilter.java
+++ take4strelka/src/nz/org/take/strelka/filter/SubstituteRealFilter.java
@@ -17,7 +17,8 @@ import nz.org.take.r2ml.R2MLException;
  * @author Bastian Schenke (bastian.schenke(at)googlemail.com)
  * 
  */
-public class SubstituteRealFilter implements nz.org.take.r2ml.util.RuleBaseFilter {
+public class SubstituteRealFilter implements
+		nz.org.take.r2ml.util.RuleBaseFilter {
 
 	/*
 	 * (non-Javadoc)
@@ -27,12 +28,14 @@ public class SubstituteRealFilter implements nz.org.take.r2ml.util.RuleBaseFilte
 	public void repair(RuleBase ruleBase) throws R2MLException {
 		JXPathContext context = JXPathContext.newContext(ruleBase);
 
-		// here is an error in the return value of the QName.getLocalPart() method.
-		// it should return only the localpart, but does returns also the namespace-prefix
+		// here is an error in the return value of QName.getLocalPart()
+		// introduced by strelka.
+		// it should return only the localpart, but does returns also the
+		// namespace-prefix
 		Iterator stuff2replace = context
-			.iterate("//*[datatypeID/localPart='xs:real']");
-//		Iterator stuff2replace = context
-//			.iterate("//*[datatypeID/localPart='real']");
+				.iterate("//*[datatypeID/localPart='xs:real']");
+		// Iterator stuff2replace = context
+		// .iterate("//*[datatypeID/localPart='real']");
 
 		while (stuff2replace.hasNext()) {
 			Object obj = stuff2replace.next();
@@ -42,6 +45,9 @@ public class SubstituteRealFilter implements nz.org.take.r2ml.util.RuleBaseFilte
 						javax.xml.XMLConstants.W3C_XML_SCHEMA_NS_URI, "double",
 						"xs"));
 			} else {
+				System.out
+						.println("Unable to replace xs:real occurence. Modify SubstituteRealFilter to handle "
+								+ obj.getClass().getCanonicalName());
 				throw new R2MLException(
 						"Unable to replace xs:real occurence. Modify SubstituteRealFilter to handle "
 								+ obj.getClass().getCanonicalName());
