b99276d318c81adbe5b4931a6464bbaf7b76d496
bastian.schenke <bastian.schenke@7b52cedb-bc2e-0410-92dc-b5e457b4923a>
2008-11-08 13:56:33 +0000

    testScenario poiunting out NullPointerExceptions from Beans

---
+++ take/src/test/nz/org/take/compiler/issue16/Bean.java
@@ -0,0 +1,61 @@
+/**
+ * Copyright 2007 Jens Dietrich Licensed under the Apache License, Version 2.0 (the "License"); 
+ * you may not use this file except in compliance with the License. 
+ * You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0 
+ * Unless required by applicable law or agreed to in writing, software distributed under the 
+ * License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, 
+ * either express or implied. See the License for the specific language governing permissions 
+ * and limitations under the License.
+ */
+
+package test.nz.org.take.compiler.issue16;
+
+/**
+ * Bean class referenced in tests. 
+ * @author <a href="http://www-ist.massey.ac.nz/JBDietrich/">Jens Dietrich</a>
+ */
+
+public class Bean {
+	private String name = null;
+
+	public Bean(String name) {
+		super();
+		this.name = name;
+	}
+	public Bean() {
+		super();
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	@Override
+	public int hashCode() {
+		final int PRIME = 31;
+		int result = 1;
+		result = PRIME * result + ((name == null) ? 0 : name.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		final Bean other = (Bean) obj;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		return true;
+	}
+
+
+}
